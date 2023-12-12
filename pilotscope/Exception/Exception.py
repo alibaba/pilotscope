db53826c1cd10986d430c53db4cc23333c9894b6
理文 <weiwenqing.wwq@alibaba-inc.com>
2023-12-12 12:50:39 +0000

    feat: raise error when sub-plan lost. fix: spark when pull time

--- pilotscope/Exception/Exception.py
+++ pilotscope/Exception/Exception.py
@@ -16,3 +16,7 @@ class DatabaseCrashException(Exception):
 class DatabaseStartException(Exception):
     def __init__(self, message="Can't start Database. Recover config and start success"):
         super().__init__(message)
+
+class PilotScopeInternalError(Exception):
+    def __init__(self, message=""):
+        super().__init__("An internal error of pilotscope has occurred. Please submit an issue to contact us for a fix. Error info is: "+message)
--- test_pilotscope/test_spark/test_spark_data_interactor.py
+++ test_pilotscope/test_spark/test_spark_data_interactor.py
@@ -70,7 +70,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_pull_record(self):
         self.data_interactor.pull_record()
-        result = self.data_interactor.execute(self.sql2)
+        result = self.data_interactor.execute(self.sql_timestamp)
         print(result)
 
     def test_push_card(self):
@@ -84,25 +84,45 @@ class MyTestCase(unittest.TestCase):
         self.data_interactor.push_card(subquery_2_value)
         result = self.data_interactor.execute(self.sql_timestamp)
         print(result)
+        
+    def test_push_card_and_pull_time(self):
+        # return # Can not pass. stuck in receiving data
+        subquery_2_value = {
+            "SELECT COUNT(*) FROM posts AS p WHERE ((((p.CreationDate IS NOT NULL) AND (p.Score IS NOT NULL)) AND ((p.CreationDate >= 1279570117) AND (p.Score < 50))) AND (p.Id IS NOT NULL)) ": 1234,
+            "SELECT COUNT(*) FROM postlinks AS pl WHERE (pl.PostId IS NOT NULL) ": 2345,
+            "SELECT COUNT(*) FROM posthistory AS ph WHERE (((ph.CreationDate IS NOT NULL) AND (ph.CreationDate >= 1279585800)) AND (ph.PostId IS NOT NULL)) ": 3456,
+            "SELECT COUNT(*) FROM posts AS p, postlinks AS pl WHERE ((((p.CreationDate IS NOT NULL) AND (p.Score IS NOT NULL)) AND ((p.CreationDate >= 1279570117) AND (p.Score < 50))) AND (p.Id IS NOT NULL)) AND  (pl.PostId IS NOT NULL) AND  (p.Id = pl.PostId) ": 4567,
+            "SELECT COUNT(*) FROM postlinks AS pl, posthistory AS ph WHERE (pl.PostId IS NOT NULL) AND  (((ph.CreationDate IS NOT NULL) AND (ph.CreationDate >= 1279585800)) AND (ph.PostId IS NOT NULL)) AND  (pl.PostId = ph.PostId) ": 5678
+        }
+        self.data_interactor.push_card(subquery_2_value)
+        self.data_interactor.pull_execution_time()
+        result = self.data_interactor.execute(self.sql_timestamp)
+        print(result)
+        
+    def test_pull_record_and_time(self):
+        self.data_interactor.pull_record()
+        self.data_interactor.pull_execution_time()
+        result = self.data_interactor.execute(self.sql_timestamp)
+        print(result)
 
-    def test_push_card_pull_physical_plan(self):
+    def test_push_card_pull_physical_plan_and_time(self):
         self.data_interactor.pull_subquery_card()
         self.data_interactor.pull_physical_plan()
+        self.data_interactor.pull_execution_time()
         result1 = self.data_interactor.execute(self.sql_timestamp)
         print("origin physical plan:\n", result1.physical_plan)
         subquery_2_value = {
-            "SELECT COUNT(*) FROM posts AS p WHERE ((((p.CreationDate IS NOT NULL) AND (p.Score IS NOT NULL)) AND ((p.CreationDate >= 1279570117) AND (p.Score < 50))) AND (p.Id IS NOT NULL)) ": 11,
-            "SELECT COUNT(*) FROM postlinks AS pl WHERE (pl.PostId IS NOT NULL) ": 22,
-            "SELECT COUNT(*) FROM posthistory AS ph WHERE (((ph.CreationDate IS NOT NULL) AND (ph.CreationDate >= 1279585800)) AND (ph.PostId IS NOT NULL)) ": 33,
-            "SELECT COUNT(*) FROM posts AS p, postlinks AS pl WHERE ((((p.CreationDate IS NOT NULL) AND (p.Score IS NOT NULL)) AND ((p.CreationDate >= 1279570117) AND (p.Score < 50))) AND (p.Id IS NOT NULL)) AND  (pl.PostId IS NOT NULL) AND  (p.Id = pl.PostId) ": 48,
-            "SELECT COUNT(*) FROM postlinks AS pl, posthistory AS ph WHERE (pl.PostId IS NOT NULL) AND  (((ph.CreationDate IS NOT NULL) AND (ph.CreationDate >= 1279585800)) AND (ph.PostId IS NOT NULL)) AND  (pl.PostId = ph.PostId) ": 53,
+            "SELECT COUNT(*) FROM posts AS p WHERE ((((p.CreationDate IS NOT NULL) AND (p.Score IS NOT NULL)) AND ((p.CreationDate >= 1279570117) AND (p.Score < 50))) AND (p.Id IS NOT NULL)) ": 11000,
+            "SELECT COUNT(*) FROM postlinks AS pl WHERE (pl.PostId IS NOT NULL) ": 22000,
+            "SELECT COUNT(*) FROM posthistory AS ph WHERE (((ph.CreationDate IS NOT NULL) AND (ph.CreationDate >= 1279585800)) AND (ph.PostId IS NOT NULL)) ": 33000,
+            "SELECT COUNT(*) FROM posts AS p, postlinks AS pl WHERE ((((p.CreationDate IS NOT NULL) AND (p.Score IS NOT NULL)) AND ((p.CreationDate >= 1279570117) AND (p.Score < 50))) AND (p.Id IS NOT NULL)) AND  (pl.PostId IS NOT NULL) AND  (p.Id = pl.PostId) ": 48000,
+            "SELECT COUNT(*) FROM postlinks AS pl, posthistory AS ph WHERE (pl.PostId IS NOT NULL) AND  (((ph.CreationDate IS NOT NULL) AND (ph.CreationDate >= 1279585800)) AND (ph.PostId IS NOT NULL)) AND  (pl.PostId = ph.PostId) ": 53000,
         }
         self.data_interactor.push_card(subquery_2_value)
         self.data_interactor.pull_physical_plan()
+        self.data_interactor.pull_execution_time()
         result2 = self.data_interactor.execute(self.sql_timestamp)
         print("replaced physical plan:\n", result2.physical_plan)
-        print(result1.physical_plan == result2.physical_plan)
-
 
 if __name__ == '__main__':
     unittest.main()
