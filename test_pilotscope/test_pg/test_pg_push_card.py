c4247cbcff5059c631b9ad4352a79a6ae4104a9e
彭佳真 <pengjiazhen.pjz@alibaba-inc.com>
2023-12-15 00:30:06 +0800

    update test cases of subquery

--- test_pilotscope/test_pg/test_pg_push_card.py
+++ test_pilotscope/test_pg/test_pg_push_card.py
@@ -4,37 +4,60 @@ import random
 from pilotscope.DBInteractor.PilotDataInteractor import PilotDataInteractor
 from pilotscope.PilotConfig import PostgreSQLConfig
 from pilotscope.PilotTransData import PilotTransData
-
+from pilotscope.Exception.Exception import PilotScopeInternalError
 
 class MyTestCase(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls):
-        cls.config = PostgreSQLConfig()
-        cls.config.db = "stats_tiny"
-        cls.data_interactor = PilotDataInteractor(cls.config)
-        cls.sql = "select count(*) from badges as b, comments as c, users as u, votes as v where b.userid = c.userid and c.userid = u.id and u.id = v.userid and b.date >= 1279574948 and c.score = 0 and u.creationdate <= 1410173533 and u.creationdate >= 1279581345 and u.reputation <= 305 and u.views <= 77 and v.bountyamount >= 0 and v.votetypeid = 2;"
-        cls.data_interactor.pull_subquery_card()
-        cls.data_interactor.pull_estimated_cost()
-        cls.data_interactor.pull_physical_plan()
-        cls.origin_result = cls.data_interactor.execute(cls.sql)
-        
+    def setUpClass(self):
+        self.config = PostgreSQLConfig()
+        self.config.db = "stats_tiny"
+        self.data_interactor = PilotDataInteractor(self.config)
+        self.sql = "select count(*) from  badges as b, comments as c, users as u, votes as v where b.userid = c.userid and c.userid = u.id and u.id = v.userid and b.date >= 1279574948 and c.score = 0 and u.creationdate <= 1410173533 and u.creationdate >= 1279581345 and u.reputation <= 305 and u.views <= 77 and v.bountyamount >= 0 and v.votetypeid = 2;"
+        self.data_interactor.pull_subquery_card(enable_parameterized_subquery=False)
+        self.data_interactor.pull_estimated_cost()
+        self.data_interactor.pull_physical_plan()
+        self.origin_result = self.data_interactor.execute(self.sql)
+    
+    def _test_push_card_to_cost(self, factor, enable_parameterized_subquery):
+        new_cards = {k:max(1,v*factor) for k,v in self.origin_result.subquery_2_card.items()}
+        self.data_interactor.push_card(new_cards, enable_parameterized_subquery)
+        self.data_interactor.pull_estimated_cost()
+        result = self.data_interactor.execute(self.sql)
+        print("factor: ", factor, " cost is ",result.estimated_cost)
+        return result.estimated_cost
         
     def test_push_card_to_cost(self):
-        larger_card = {k:v*10000 for k,v in self.origin_result.subquery_2_card.items()}
-        self.data_interactor.push_card(larger_card)
-        self.data_interactor.pull_estimated_cost()
-        result = self.data_interactor.execute(self.sql)
-        print("cost is ",result.estimated_cost,". before push_card, cost is",self.origin_result.estimated_cost)
-        self.assertTrue(result.estimated_cost>self.origin_result.estimated_cost*100)
+        print("\n")
+        print("factor: ", 1, " cost is ",self.origin_result.estimated_cost)
+        factor = 1
+        costs = [self.origin_result.estimated_cost]
+        for i in range(10):
+            factor *= 10
+            cost = self._test_push_card_to_cost(factor, False)
+            costs.append(cost)
+        self.assertTrue(all(costs[i] <= costs[i+1] for i in range(len(costs)-1))) # check costs is non-decreasing.
         
-        smaller_card = {k:max(1,v/100) for k,v in self.origin_result.subquery_2_card.items()}
-        self.data_interactor.push_card(smaller_card)
-        self.data_interactor.pull_estimated_cost()
-        result = self.data_interactor.execute(self.sql)
-        print("cost is ",result.estimated_cost,". before push_card, cost is",self.origin_result.estimated_cost)
-        self.assertTrue(result.estimated_cost<self.origin_result.estimated_cost*2)
-        
+        print("\n")
+        factor = 1
+        costs = []
+        for i in range(10):
+            factor *= 10
+            cost = self._test_push_card_to_cost(1/factor, False)
+            costs.append(cost)
+        self.assertTrue(all(costs[i] >= costs[i+1] for i in range(len(costs)-1))) # check costs is non-increasing.
+            
+    def test_enable_parameterized_subquery_consistency(self):
+        self.data_interactor.push_card(self.origin_result.subquery_2_card, True)
+        self.data_interactor.pull_physical_plan()
+        # self.data_interactor.execute(self.sql)
+        try:
+            self.data_interactor.execute(self.sql)
+            self.assertTrue(False, "enable_parameterized_subquery is inconsistency in pull_subquery and push_card, it should be an error.")
+        except Exception as e:
+            self.assertEqual(type(e), PilotScopeInternalError)
+            self.assertTrue("Can not find the corresponding sub-plan query in push anchor", str(e))
+    
     def test_card_consistency(self):
         model_subquery_2_card = {}
         for i, subquery in enumerate(self.origin_result.subquery_2_card):
--- test_pilotscope/test_pg/test_pg_subquery.py
+++ test_pilotscope/test_pg/test_pg_subquery.py
@@ -17,96 +17,157 @@ class MyTestCase(unittest.TestCase):
         self.data_interactor = PilotDataInteractor(self.config)
         self.db_controller: PostgreSQLController = DBControllerFactory.get_db_controller(self.config)
 
-    def _check_sq(self, sql, results):
-        self.data_interactor.pull_subquery_card()
+    def _check_sq(self, sql, results, enable_parameterized_subquery=False):
+        self.data_interactor.pull_subquery_card(enable_parameterized_subquery)
         result: PilotTransData = self.data_interactor.execute(sql)
         subquery_2_card = result.subquery_2_card
         self.assertFalse(subquery_2_card is None or len(subquery_2_card) == 0)
-        self.assertEqual(len(subquery_2_card), len(results), "The count of subqueries is incorrect.")
         print("[Test SQL] ", sql)
+        sqs = []
         for i, (sq, card) in enumerate(subquery_2_card.items()):
-            print(f"\tSQ {i + 1}: ", sq, card)
+            print(f"\tSQ {i+1}: ", sq, card)
+            if sq.count("/*") == 1:  # not parameterized path
+                sqs.append(sq)
+            else:      # parameterized path
+                sq = sq.split("; ")[:-2]
+                for q in sq:
+                    sqs.append(q)
+
+        self.assertEqual(len(sqs), len(results), "The count of subqueries is incorrect.")
+        for i, sq in enumerate(sqs):
             try:
                 parse_one(sq)
             except Exception as e:
-                print("\tParse ERROR")
+                print("\tParse ERROR", sq)
                 self.fail(e)
             result1 = self.db_controller.execute(sq, fetch=True)
-            # if "count" not in results[i].lower():
-            #     results[i] = results[i].replace("*", "COUNT(*)")
             result2 = self.db_controller.execute(results[i], fetch=True)
-
-            if result1 != result2:
-                print("\tSQ ERROR")
-            # self.assertEqual(result1, result2, "SQ ERROR")
-            self.assertTrue(result1 == result2, "SQ ERROR")
+            self.assertTrue(result1 == result2, f"SQ ERROR, {i+1}: {sq}, {results[i]}")
 
     def test_contains_string(self):
+        print("\n[Test Type String]")
         sql = "select * from company_name as cn where cn.country_code = '[ru]';"
         results = ["select COUNT(*) from company_name as cn where cn.country_code = '[ru]';"]
         self._check_sq(sql, results)
-
+    
     def test_like(self):
+        print("\n[Test keyword Like]")
         sql = "select * from cast_info ci where ci.note like '%(voice)%';"
         results = ["select COUNT(*) from cast_info ci where ci.note like '%(voice)%';"]
         self._check_sq(sql, results)
-
+    
     def test_between(self):
+        print("\n[Test keyword Between]")
         sql = "select * from title t where t.production_year between 2007 and 2010;"
         results = ["select COUNT(*) from title t where t.production_year between 2007 and 2010;"]
         self._check_sq(sql, results)
-
+    
     def test_in(self):
+        print("\n[Test keyword In]")
         sql = "select * from keyword k where k.keyword in ('sequel', 'revenge', 'based-on-novel');"
         results = ["select COUNT(*) from keyword k where k.keyword in ('sequel', 'revenge', 'based-on-novel');"]
         self._check_sq(sql, results)
-
+    
     def test_is_null(self):
+        print("\n[Test operation \'is Null\']")
         sql = "select * from movie_companies mc where mc.note is Null;"
         results = ["select COUNT(*) from movie_companies mc where mc.note is Null;"]
         self._check_sq(sql, results)
-
+    
     def test_and_or(self):
+        print("\n[Test And OR]")
         sql = "select * from company_name as cn WHERE cn.country_code !='[pl]' and (cn.name like '%Film%' or cn.name like '%Warner%');"
-        results = [
-            "select COUNT(*) from company_name as cn WHERE cn.country_code !='[pl]' and (cn.name like '%Film%' or cn.name like '%Warner%');"]
+        results = ["select COUNT(*) from company_name as cn WHERE cn.country_code !='[pl]' and (cn.name like '%Film%' or cn.name like '%Warner%');"]
         self._check_sq(sql, results)
 
     def test_inner_join(self):
+        print("\n[Test Inner Join]")
         sql = "select * from title t,movie_keyword mk where t.id=mk.movie_id and mk.keyword_id=117;"
         results = [
             "SELECT COUNT(*) FROM title t;",
             "SELECT COUNT(*) FROM movie_keyword mk WHERE (mk.keyword_id = 117);",
-            "select COUNT(*) from title t,movie_keyword mk where t.id=mk.movie_id and mk.keyword_id=117;",
+            "SELECT COUNT(*) FROM title t;",
+            "SELECT COUNT(DISTINCT mk.movie_id) FROM movie_keyword mk;",
+            "SELECT COUNT(*) FROM movie_keyword mk WHERE (mk.keyword_id = 117);",
+            "SELECT COUNT(DISTINCT t.id) FROM title t;",
+            "SELECT COUNT(*) FROM title t, movie_keyword mk WHERE (t.id = mk.movie_id) AND (mk.keyword_id = 117);",
         ]
-        self._check_sq(sql, results)
-
+        non_param_index = [0,1,6]
+        print("-- test normal subquery")
+        self._check_sq(sql, [results[i] for i in non_param_index], False)
+        print("-- test parameterized subquery")
+        self._check_sq(sql, results, True)
+    
     def test_left_join(self):
+        print("\n[Test Left Join]")
         sql = "select * from title t LEFT JOIN movie_keyword mk on t.id=mk.movie_id where t.production_year=2008;"
         results = [
             "SELECT COUNT(*) FROM title t WHERE (t.production_year = 2008);",
             "SELECT COUNT(*) FROM movie_keyword mk;",
-            "select COUNT(*) from title t LEFT JOIN movie_keyword mk on t.id=mk.movie_id where t.production_year=2008;",
+            "SELECT COUNT(*) FROM movie_keyword mk;",
+            "SELECT COUNT(DISTINCT t.id) FROM title t;",
+            "SELECT COUNT(*) FROM title t LEFT JOIN movie_keyword mk ON t.id = mk.movie_id WHERE (t.production_year = 2008);",
         ]
-        self._check_sq(sql, results)
-
+        non_param_index = [0,1,4]
+        print("-- test normal subquery")
+        self._check_sq(sql, [results[i] for i in non_param_index], False)
+        print("-- test parameterized subquery")
+        self._check_sq(sql, results, True)
+        
     def test_right_join(self):
+        print("\n[Test Right Join]")
         sql = "select * from movie_keyword mk Right JOIN title t on t.id=mk.movie_id;"
         results = [
             "SELECT COUNT(*) FROM movie_keyword mk",
             "SELECT COUNT(*) FROM title t;",
-            "select COUNT(*) from movie_keyword mk Right JOIN title t on t.id=mk.movie_id;",
+            "SELECT COUNT(*) FROM movie_keyword mk",
+            "SELECT COUNT(DISTINCT t.id) FROM title t;",
+            "select COUNT(*) FROM movie_keyword mk Right JOIN title t ON t.id=mk.movie_id;",
         ]
-        self._check_sq(sql, results)
-
+        non_param_index = [0,1,4]
+        print("-- test normal subquery")
+        self._check_sq(sql, [results[i] for i in non_param_index], False)
+        print("-- test parameterized subquery")
+        self._check_sq(sql, results, True)
+    
     def test_full_join(self):
+        print("\n[Test Full Join]")
         sql = "select * from title t FULL JOIN movie_keyword mk on t.id=mk.movie_id where t.production_year=2008;"
         results = [
             "SELECT COUNT(*) FROM title t WHERE (t.production_year = 2008);",
             "SELECT COUNT(*) FROM movie_keyword mk;",
-            "select COUNT(*) from title t FULL JOIN movie_keyword mk on t.id=mk.movie_id where t.production_year=2008;",
+            "SELECT COUNT(*) FROM movie_keyword mk;",
+            "SELECT COUNT(DISTINCT t.id) FROM title t;",
+            "select COUNT(*) from title t FULL JOIN movie_keyword mk ON t.id=mk.movie_id where t.production_year=2008;",
         ]
-        self._check_sq(sql, results)
+        non_param_index = [0,1,4]
+        print("-- test normal subquery")
+        self._check_sq(sql, [results[i] for i in non_param_index], False)
+        print("-- test parameterized subquery")
+        self._check_sq(sql, results, True)
+    
+    def test_mix_join(self):
+        print("\n[Test Mix Join]")
+        sql = "select * from movie_companies mc, title t LEFT JOIN movie_keyword mk on t.id=mk.movie_id where t.id=mc.movie_id and t.production_year > 2007;"
+        results = [
+            "SELECT COUNT(*) FROM movie_companies mc;",
+            "SELECT COUNT(*) FROM title t WHERE (t.production_year > 2007);",
+            "SELECT COUNT(*) FROM movie_keyword mk;",
+            "SELECT COUNT(*) FROM movie_companies mc;",
+            "SELECT COUNT(DISTINCT t.id) FROM title t;",
+            "SELECT COUNT(*) FROM title t WHERE (t.production_year > 2007);",
+            "SELECT COUNT(DISTINCT mc.movie_id) FROM movie_companies mc;",
+            "SELECT COUNT(*) FROM movie_keyword mk;",
+            "SELECT COUNT(DISTINCT t.id) FROM title t;",
+            "SELECT COUNT(*) FROM movie_companies mc, title t WHERE (mc.movie_id = t.id) AND (t.production_year > 2007);",
+            "SELECT COUNT(*) FROM title t LEFT JOIN movie_keyword mk ON t.id = mk.movie_id WHERE (t.production_year > 2007);",
+            "SELECT COUNT(*) FROM movie_companies mc, title t LEFT JOIN movie_keyword mk ON t.id=mk.movie_id WHERE t.id=mc.movie_id AND t.production_year > 2007;",
+        ]
+        non_param_index = [0,1,2,9,10,11]
+        print("-- test normal subquery")
+        self._check_sq(sql, [results[i] for i in non_param_index], False)
+        print("-- test parameterized subquery")
+        self._check_sq(sql, results, True)
 
 
 if __name__ == '__main__':
